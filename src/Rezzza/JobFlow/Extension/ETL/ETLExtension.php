6f025e29ddf7bd08bf497cecd61525dda586be19
Timothée Barray <tim@amicalement-web.net>
2013-09-03 16:09:00 +0200

    Add DataMapper Transformer

--- src/Rezzza/JobFlow/Extension/ETL/ETLExtension.php
+++ src/Rezzza/JobFlow/Extension/ETL/ETLExtension.php
@@ -12,8 +12,11 @@ class ETLExtension extends BaseExtension
             new Type\Extractor\ExtractorType(),
             new Type\Extractor\CsvExtractorType(),
             new Type\Extractor\TsvExtractorType(),
+
             new Type\Transformer\TransformerType(),
             new Type\Transformer\CallbackTransformerType(),
+            new Type\Transformer\DataMapperTransformerType(),
+            
             new Type\Loader\LoaderType(),
             new Type\Loader\FileLoaderType()
         );
---
+++ src/Rezzza/JobFlow/Extension/ETL/Type/Transformer/DataMapperTransformerType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Rezzza\JobFlow\Extension\ETL\Type\Transformer;
+
+use Symfony\Component\OptionsResolver\Options;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+use Rezzza\JobFlow\AbstractJobType;
+
+class DataMapperTransformerType extends AbstractJobType
+{
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'class' => 'Knp\ETL\Transformer\DataMap',
+            'args' => function(Options $options) {
+                return $options['mapping'];
+            }
+        ));
+
+        $resolver->setRequired(array(
+            'mapping'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'datamapper_transformer';
+    }
+
+    public function getParent()
+    {
+        return 'transformer';
+    }
+}
