34b308e890160d0b9a2f4e7bafd990ea9511a8a8
Timothée Barray <tim@amicalement-web.net>
2013-09-04 11:35:37 +0200

    fix EntityLoaderType

--- src/Rezzza/JobFlow/Extension/Doctrine/Type/Loader/EntityLoader.php
+++ src/Rezzza/JobFlow/Extension/Doctrine/Type/Loader/EntityLoader.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Rezzza\JobFlow\Extension\Doctrine\ORM;
+namespace Rezzza\JobFlow\Extension\Doctrine\Type\Loader;
 
 use Doctrine\Common\Persistence\ManagerRegistry;
 use Symfony\Component\OptionsResolver\Options;
@@ -14,7 +14,7 @@ class EntityLoader extends AbstractJobType
 
     public function __construct(ManagerRegistry $doctrine)
     {
-        $this->doctrine = $doctrine:
+        $this->doctrine = $doctrine;
     }
 
     public function setDefaultOptions(OptionsResolverInterface $resolver)
