f409f2339f544365d566fbc7be19576d4471a514
Timothée Barray <tim@amicalement-web.net>
2013-09-10 11:09:18 +0200

    Improve basic example

--- examples/basic.php
+++ examples/basic.php
@@ -4,26 +4,37 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 use Rezzza\JobFlow\Jobs;
 use Rezzza\JobFlow\Io;
+use Rezzza\JobFlow\Extension\ETL\Type;
 
-$logger = new \Monolog\Logger('jobflow');
+// Create the JobFactory. 
+// By default it comes with CoreExtension and ETLExtension
+// If you need to inject others extensions :
+// $builder = $Jobs::createJobFactoryBuilder();
+// $builder->addExtension(nex MyExtension());
 $jobFactory = Jobs::createJobFactory();
 
-$scheduler = $jobFactory->createJobFlow('php');
-$scheduler->setLogger($logger);
+// Create the scheduler responsible for the job execution
+$jobflow = $jobFactory->createJobFlow('php');
 
+// We can inject Logger
+$jobflow->setLogger(new \Monolog\Logger('jobflow'));
+
+// We will inject IO to our job to indicate where Extractor needs to read and where Loader needs to write
 $io = new Io\IoDescriptor(
     new Io\Input('file://'.__DIR__.'/fixtures.csv'),
     new Io\Output('file:///'.__DIR__.'/temp/result.json')
 );
 
-$job = $jobFactory->createBuilder('job', $io)
+// Here we go, you can build job on the fly
+$job = $jobFactory
+    ->createBuilder('job', $io) // 'job' is the JobType by default.
     ->add(
-        'example_extractor', 
-        'csv_extractor'
+        'example_extractor', // name
+        new Type\Extractor\CsvExtractorType() // or you can use 'csv_extractor' as ETLExtension is loaded by default
     )
     ->add(
-        'example_transformer', 
-        'callback_transformer',
+        'example_transformer', // name
+        new Type\Transformer\CallbackTransformerType(), // or 'callback_transformer'
         array(
             'callback' => function($data, $target) {
                 $target['firstname'] = $data[0];
@@ -35,14 +46,15 @@ $job = $jobFactory->createBuilder('job', $io)
         )
     )
     ->add(
-        'example_loader',
-        'file_loader'
+        'example_loader', // name
+        new Type\Loader\FileLoaderType() // or 'file_loader'
     )
-    ->getJob()
+    ->getJob() // builder create the Job with this method
 ;
 
-$scheduler
+// Now we can execute our job
+$jobflow
     ->setJob($job)
-    ->init()
+    ->init() // Will create the first message to run the process
     ->run()
 ;
