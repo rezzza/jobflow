23d9271162da6147ea2b393d9fd0bbc57d6c4aea
Timothée Barray <tim@amicalement-web.net>
2014-04-15 17:51:30 +0200

    update tests : ExtractorProxy

--- tests/fixtures/DummyExtractor.php
+++ tests/fixtures/DummyExtractor.php
@@ -5,10 +5,38 @@ namespace Rezzza\Jobflow\Tests\Fixtures;
 use Knp\ETL\ContextInterface;
 use Knp\ETL\ExtractorInterface;
 
-class DummyExtractor implements ExtractorInterface
+class DummyExtractor implements ExtractorInterface, \Iterator, \Countable
 {
     public function extract(ContextInterface $context)
     {
         // Who's care ?
+    }
+
+    public function current()
+    {
+    }
+
+    public function key()
+    {
+    }
+
+    public function next()
+    {
+    }
+
+    public function rewind()
+    {
+    }
+
+    public function valid()
+    {
+    }
+
+    public function count()
+    {
+    }
+
+    public function seek()
+    {
     }
 }
